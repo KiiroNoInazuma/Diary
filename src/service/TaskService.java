c859482394773924710d0bc0bfa516e71f06e4b0
John Rey <adioskid@mail.ru>
2024-08-10 21:45:42 +0400

    process

--- src/service/TaskService.java
+++ src/service/TaskService.java
@@ -2,6 +2,7 @@ package service;
 
 import entities.Task;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 
@@ -9,5 +10,17 @@ public class TaskService {
 
     private Map<Integer, Task> taskMap;
     private Collection<Task> removedTasks;
+
+    public void addTask(Task task){
+
+    }
+
+    public Task remove(Task task) {
+        return null;
+    }
+
+    Collection<Task> getAllByDate(LocalDate date) {
+        return null;
+    }
 
 }
